b37dd951f2bf44ad817e8714923f9dd4fc9f3146
Cuasare <kauantv554@gmail.com>
2026-06-25 16:37:45 -0400

    feat: conexão com banco de dados

---
+++ backend/app/db/database.py
@@ -0,0 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+Base = declarative_base()
+
+async def get_async_session() -> AsyncSession:
+    async with AsyncSessionLocal as session:
+        yield session
